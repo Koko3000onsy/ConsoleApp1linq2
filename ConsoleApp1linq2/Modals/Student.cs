675dd407c9ee1a115a3fe37125cdc1a1a0c99372
Koko3000onsy <onsykoko79@gmail.com>
2026-04-16 14:43:05 +0200

    Add project files.

---
+++ ConsoleApp1linq2/Modals/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1linq2.Modals
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public string Major { get; set; }
+    }
+}
---
+++ ConsoleApp1linq2/Program.cs
@@ -0,0 +1,11 @@
+﻿using static ConsoleApp1linq2.DataSource.Source;
+namespace ConsoleApp1linq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              
+        }
+    }
+}
